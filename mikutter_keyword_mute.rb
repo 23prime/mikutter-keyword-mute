ccd9fdb92b77ea7e07b77571b675d4a1a495f7f3
23prime <23.prime.37@gmail.com>
2019-04-14 22:18:54 +0900

    Correspond to Regexp

--- mikutter_keyword_mute.rb
+++ mikutter_keyword_mute.rb
@@ -6,8 +6,24 @@ Plugin.create(:mute_word) do
     multi('指定した単語を含むツイートをミュートします', :muted_words)
   end
 
+  def include?(msg, word)
+    msg_s = msg.to_s
+    case_ignore = false
+
+    if word =~ %r{^/}
+      if word =~ %r{/i$}
+        word = word[1..-3]
+        case_ignore = true
+      elsif word =~ %r{/$}
+        word = word[1..-2]
+      end
+    end
+    return msg_s =~ Regexp.new(word, Regexp::IGNORECASE) if case_ignore
+    return msg_s =~ Regexp.new(word)
+  end
+
   filter_show_filter do |msgs|
-    msgs = msgs.reject { |msg| exclude_words.any? { |word| msg.to_s.include?(word) } }
+    msgs = msgs.reject { |msg| exclude_words.any? { |word| include?(msg, word) } }
     [msgs]
   end
 end
